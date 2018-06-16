8b688acf6932687f62df9a4376cd856d989b52a0
Dirk Stolle <striezel-dev@web.de>
2018-06-16 16:02:18 +0200

    add missing include to TaskManager.cpp

--- src/tasks/TaskManager.cpp
+++ src/tasks/TaskManager.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the weather information collector.
-    Copyright (C) 2017  Dirk Stolle
+    Copyright (C) 2017, 2018  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 */
 
 #include "TaskManager.hpp"
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <map>
