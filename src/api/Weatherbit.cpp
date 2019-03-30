887af282af778c0ebd56cbd27d3e3f8340e4728d
Dirk Stolle <striezel-dev@web.de>
2019-03-31 00:38:45 +0100

    add class to handle Weatherbit API requests

---
+++ src/api/Weatherbit.cpp
@@ -0,0 +1,172 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the weather information collector.
+    Copyright (C) 2019  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "Weatherbit.hpp"
+#include <cmath>
+#include <iostream>
+#ifndef wic_no_json_parsing
+#include "../json/NLohmannJsonWeatherbit.hpp"
+#include "../net/Curly.hpp"
+#endif // wic_no_json_parsing
+#include "../util/Strings.hpp"
+
+namespace wic
+{
+
+Weatherbit::Weatherbit(const std::string& key)
+: m_apiKey(key)
+{
+}
+
+void Weatherbit::setApiKey(const std::string& key)
+{
+  m_apiKey = key;
+}
+
+bool Weatherbit::validLocation(const Location& location) const
+{
+  // Only latitude and longitude are required for a request.
+  return location.hasCoordinates();
+  // TODO: Add checks for other possibilities.
+}
+
+bool Weatherbit::supportsDataType(const DataType data) const
+{
+  // Only current weather data and forecast can be retrieved, not both together.
+  switch (data)
+  {
+    case DataType::Current:
+    case DataType::Forecast:
+         return true;
+    case DataType::CurrentAndForecast:
+    case DataType::none:
+    default:
+         return false;
+  } // switch
+}
+
+std::string Weatherbit::toRequestString(const Location& location) const
+{
+  if (location.hasCoordinates())
+    return "lat=" + std::to_string(location.latitude())+ std::string("&lon=")
+         + std::to_string(location.longitude());
+  // TODO: Add query string for other possibilities.
+  /*if (location.hasName() && location.hasCountry())
+    return "city=" + std::to_string(location.name())+ std::string("&country=")
+         + std::to_string(location.country()); */
+  // no required data set
+  return std::string();
+}
+
+#ifndef wic_no_json_parsing
+bool Weatherbit::parseCurrentWeather(const std::string& json, Weather& weather) const
+{
+  return NLohmannJsonWeatherbit::parseCurrentWeather(json, weather);
+}
+
+bool Weatherbit::currentWeather(const Location& location, Weather& weather)
+{
+  weather = Weather();
+  if (m_apiKey.empty())
+    return false;
+  // https://api.weatherbit.io/v2.0/current?key=API-key-here&city=city-name-here&country=ISO-3166-two-letter-code-here
+  const std::string url = "https://api.weatherbit.io/v2.0/current?key=" + m_apiKey
+                        // Use the metric system, because we don't want any quarter pounder with cheese today.
+                        + std::string("&units=M")
+                        + "&" + toRequestString(location);
+  std::string response;
+  {
+    Curly curly;
+    curly.setURL(url);
+
+    if (!curly.perform(response))
+    {
+      return false;
+    }
+    if (curly.getResponseCode() != 200)
+    {
+      std::cerr << "Error in Weatherbit::currentWeather(): Unexpected HTTP status code "
+                << curly.getResponseCode() << "!" << std::endl;
+      const auto & rh = curly.responseHeaders();
+      std::cerr << "HTTP response headers (" << rh.size() << "):" << std::endl;
+      for (const auto & s : rh)
+      {
+        std::cerr << "    " << s << std::endl;
+      }
+      return false;
+    }
+  } // scope of curly
+
+  // Parsing is done here.
+  return parseCurrentWeather(response, weather);
+}
+
+bool Weatherbit::parseForecast(const std::string& json, Forecast& forecast) const
+{
+  return NLohmannJsonWeatherbit::parseForecast(json, forecast);
+}
+
+bool Weatherbit::forecastWeather(const Location& location, Forecast& forecast)
+{
+  forecast = Forecast();
+  if (m_apiKey.empty())
+    return false;
+  // https://api.weatherbit.io/v2.0/forecast/3hourly?key=you-API-key-here&city=city-name-here&country=ISO-3166-two-letter-code-here
+  const std::string url = "https://api.weatherbit.io/v2.0/forecast/3hourly?key=" + m_apiKey
+                        // Use the metric system.
+                        + std::string("&units=M")
+                        + "&" + toRequestString(location);
+  std::string response;
+  {
+    Curly curly;
+    curly.setURL(url);
+
+    forecast.setRequestTime(std::chrono::system_clock::now());
+    if (!curly.perform(response))
+    {
+      return false;
+    }
+    if (curly.getResponseCode() != 200)
+    {
+      std::cerr << "Error in Weatherbit::forecastWeather(): Unexpected HTTP status code "
+                << curly.getResponseCode() << "!" << std::endl;
+      const auto & rh = curly.responseHeaders();
+      std::cerr << "HTTP response headers (" << rh.size() << "):" << std::endl;
+      for (const auto & s : rh)
+      {
+        std::cerr << "    " << s << std::endl;
+      }
+      return false;
+    }
+  } // scope of curly
+
+  // Parsing is done in another method.
+  return parseForecast(response, forecast);
+}
+
+bool Weatherbit::currentAndForecastWeather(const Location& location, Weather& weather, Forecast& forecast)
+{
+  std::cerr << "Error: Getting both current weather and forecast data in a "
+            << "single request is not supported by Weatherbit!" << std::endl;
+  return false;
+}
+#endif // wic_no_json_parsing
+
+} // namespace
---
+++ src/json/NLohmannJsonWeatherbit.cpp
@@ -0,0 +1,174 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the weather information collector.
+    Copyright (C) 2019  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "NLohmannJsonWeatherbit.hpp"
+#include <cmath>
+#include <iostream>
+
+namespace wic
+{
+
+bool NLohmannJsonWeatherbit::parseSingleWeatherItem(const value_type& value, Weather& weather)
+{
+  if (value.empty() || !value.is_object())
+    return false;
+  auto find = value.find("temp");
+  if (find != value.end() && find->is_number())
+  {
+    const float celsiusRaw = find->get<float>();
+    weather.setTemperatureCelsius(celsiusRaw);
+    // Avoid values like 5.9999... °C by rounding, if appropriate.
+    const float celsiusRounded = std::round(weather.temperatureCelsius());
+    if (std::fabs(celsiusRounded - weather.temperatureCelsius()) < 0.005)
+    {
+      weather.setTemperatureCelsius(celsiusRounded);
+    }
+    weather.setTemperatureKelvin(celsiusRaw + 273.15);
+    // Avoid values like 280.9999... K by rounding, if appropriate.
+    const float kelvinRounded = std::round(weather.temperatureKelvin());
+    if (std::fabs(kelvinRounded - weather.temperatureKelvin()) < 0.005)
+    {
+      weather.setTemperatureKelvin(kelvinRounded);
+    }
+    weather.setTemperatureFahrenheit(celsiusRaw * 1.8 + 32.0f);
+    // Avoid values like 6.9999... ° F by rounding, if appropriate.
+    const float fahrenheitRounded = std::round(weather.temperatureFahrenheit());
+    if (std::fabs(fahrenheitRounded - weather.temperatureFahrenheit()) < 0.005)
+    {
+      weather.setTemperatureFahrenheit(fahrenheitRounded);
+    }
+  }
+  find = value.find("rh");
+  if (find != value.end() && find->is_number_unsigned())
+    weather.setHumidity(find->get<int>());
+  find = value.find("precip");
+  if (find != value.end() && find->is_number())
+    weather.setRain(find->get<float>());
+  find = value.find("snow");
+  if (find != value.end() && find->is_number())
+    weather.setSnow(find->get<float>());
+  find = value.find("pres");
+  if (find != value.end() && find->is_number())
+    weather.setPressure(static_cast<int16_t>(find->get<float>()));
+  find = value.find("wind_spd");
+  if (find != value.end() && find->is_number())
+    weather.setWindSpeed(find->get<float>());
+  find = value.find("wind_dir");
+  if (find != value.end() && find->is_number_unsigned())
+    weather.setWindDegrees(static_cast<int16_t>(find->get<float>()));
+  find = value.find("clouds");
+  if (find != value.end() && find->is_number_unsigned())
+      weather.setCloudiness(find->get<int>());
+
+  find = value.find("ts");
+  if (find != value.end() && find->is_number())
+  {
+    // I'm not sure whether this really works as expected.
+    // Weatherbit's documentation says the timestamp is UTC, but tests show that
+    // this may not be the case.
+    const auto dt = std::chrono::time_point<std::chrono::system_clock>(std::chrono::seconds(find->get<int64_t>()));
+    weather.setDataTime(dt);
+  } // if ts
+  return weather.hasDataTime();
+}
+
+bool NLohmannJsonWeatherbit::parseCurrentWeather(const std::string& json, Weather& weather)
+{
+  value_type root; // will contain the root value after parsing.
+  try
+  {
+    root = nlohmann::json::parse(json);
+  }
+  catch(...)
+  {
+    std::cerr << "Error in NLohmannJsonWeatherbit::parseCurrentWeather(): Unable to parse JSON data!" << std::endl;
+    return false;
+  }
+
+  const auto foundData = root.find("data");
+  if (foundData == root.end() || !foundData->is_array() || foundData->size() != 1)
+  {
+    std::cerr << "Error in NLohmannJsonWeatherbit::parseCurrentWeather(): JSON "
+              << "does not contain a data element or data element is not an "
+              << "array containing a single element!" << std::endl;
+    return false;
+  }
+  const auto count = root.find("count");
+  if (count == root.end() || !count->is_number_unsigned())
+  {
+    std::cerr << "Error in NLohmannJsonWeatherbit::parseCurrentWeather(): JSON does not contain a count element or the element is not a valid number!" << std::endl;
+    return false;
+  }
+  if (count->get<unsigned int>() != 1)
+  {
+    std::cerr << "Error in NLohmannJsonWeatherbit::parseCurrentWeather(): Expected count to be 1, but the actual count is "
+              << count->get<unsigned int>() << "!" << std::endl;
+    return false;
+  }
+
+  weather.setJson(json);
+
+  return parseSingleWeatherItem(foundData->at(0), weather);
+}
+
+bool NLohmannJsonWeatherbit::parseForecast(const std::string& json, Forecast& forecast)
+{
+  value_type root; // will contain the root value after parsing.
+  try
+  {
+    root = nlohmann::json::parse(json);
+  }
+  catch(...)
+  {
+    std::cerr << "Error in NLohmannJsonWeatherbit::parseForecast(): Unable to parse JSON data!" << std::endl;
+    return false;
+  }
+
+  forecast.setJson(json);
+  if (root.empty())
+    return false;
+
+  const auto findData = root.find("data");
+  if ((findData == root.end()) || findData->empty() || !findData->is_array())
+  {
+    std::cerr << "Error in NLohmannJsonWeatherbit::parseForecast(): data is either empty or not an array!" << std::endl;
+    return false;
+  }
+  const value_type dataJson = *findData;
+  forecast.setData({ });
+  auto data = forecast.data();
+  for (const value_type val : dataJson)
+  {
+    Weather w;
+    if (parseSingleWeatherItem(val, w))
+    {
+      data.push_back(w);
+    }
+    else
+    {
+      std::cerr << "Error in NLohmannJsonWeatherbit::parseForecast(): Parsing single item failed!" << std::endl;
+      return false;
+    }
+  } // for (range-based)
+  forecast.setData(data);
+  return true;
+}
+
+} // namespace
