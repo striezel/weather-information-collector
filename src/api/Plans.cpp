265547b058ab6bb2b5056f1f8d60c7c214ec6339
Dirk Stolle <striezel-dev@web.de>
2020-02-24 23:40:29 +0100

    add enumeration types for API plans

---
+++ src/api/Plans.cpp
@@ -0,0 +1,133 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the weather information collector.
+    Copyright (C) 2020  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "Plans.hpp"
+#include "../util/Strings.hpp"
+
+namespace wic
+{
+
+std::string toString(const PlanOwm type)
+{
+  switch (type)
+  {
+    case PlanOwm::Free:
+         return "free";
+    case PlanOwm::Startup:
+         return "startup";
+    case PlanOwm::Developer:
+         return "developer";
+    case PlanOwm::Professional:
+         return "professional";
+    case PlanOwm::Enterprise:
+         return "enterprise";
+    case PlanOwm::none:
+    default:
+         return "none";
+  }
+}
+
+PlanOwm toPlanOwm(const std::string& planName)
+{
+  auto name = toLowerString(planName);
+  trim(name);
+  if (name == "free")
+    return PlanOwm::Free;
+  if (name == "startup")
+    return PlanOwm::Startup;
+  if (name == "developer")
+    return PlanOwm::Developer;
+  if (name == "professional")
+    return PlanOwm::Professional;
+  if (name == "enterprise")
+    return PlanOwm::Enterprise;
+  // unknown / none
+  return PlanOwm::none;
+}
+
+std::string toString(const PlanWeatherbit type)
+{
+  switch (type)
+  {
+    case PlanWeatherbit::Free:
+         return "free";
+    case PlanWeatherbit::Starter:
+         return "starter";
+    case PlanWeatherbit::Developer:
+         return "developer";
+    case PlanWeatherbit::Advanced:
+         return "advanced";
+    case PlanWeatherbit::none:
+    default:
+         return "none";
+  }
+}
+
+PlanWeatherbit toPlanWeatherbit(const std::string& planName)
+{
+  auto name = toLowerString(planName);
+  trim(name);
+  if (name == "free")
+    return PlanWeatherbit::Free;
+  if (name == "starter")
+    return PlanWeatherbit::Starter;
+  if (name == "developer")
+    return PlanWeatherbit::Developer;
+  if (name == "advanced")
+    return PlanWeatherbit::Advanced;
+  // unknown / none
+  return PlanWeatherbit::none;
+}
+
+std::string toString(const PlanWeatherstack type)
+{
+  switch (type)
+  {
+    case PlanWeatherstack::Free:
+         return "free";
+    case PlanWeatherstack::Standard:
+         return "standard";
+    case PlanWeatherstack::Professional:
+         return "professional";
+    case PlanWeatherstack::Business:
+         return "business";
+    case PlanWeatherstack::none:
+    default:
+         return "none";
+  }
+}
+
+PlanWeatherstack toPlanWeatherstack(const std::string& planName)
+{
+  auto name = toLowerString(planName);
+  trim(name);
+  if (name == "free")
+    return PlanWeatherstack::Free;
+  if (name == "standard")
+    return PlanWeatherstack::Standard;
+  if (name == "professional")
+    return PlanWeatherstack::Professional;
+  if (name == "business")
+    return PlanWeatherstack::Business;
+  // unknown / none
+  return PlanWeatherstack::none;
+}
+
+} // namespace
---
+++ src/api/Plans.hpp
@@ -0,0 +1,96 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the weather information collector.
+    Copyright (C) 2020  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef WEATHER_INFORMATION_COLLECTION_PLANS_HPP
+#define WEATHER_INFORMATION_COLLECTION_PLANS_HPP
+
+#include <string>
+
+namespace wic
+{
+
+/** \brief enumeration type for plans of OpenWeatherMap
+ */
+enum class PlanOwm { none, Free, Startup, Developer, Professional, Enterprise };
+
+
+/** \brief Gets the name of the OpenWeatherMap plan.
+ *
+ * \param type  the plan
+ * \return Returns a string that identifies the plan.
+ */
+std::string toString(const PlanOwm type);
+
+
+/** \brief Converts a name / string to a OpenWeatherMap plan.
+ *
+ * \param planName  name of the plan
+ * \return Returns the enumeration value for the plan.
+ *         Returns PlanOwm::none, if no match could be found.
+ */
+PlanOwm toPlanOwm(const std::string& planName);
+
+
+/** \brief enumeration type for plans of Weatherbit
+ */
+enum class PlanWeatherbit { none, Free, Starter, Developer, Advanced };
+
+
+/** \brief Gets the name of the Weatherbit plan.
+ *
+ * \param type  the plan
+ * \return Returns a string that identifies the plan.
+ */
+std::string toString(const PlanWeatherbit type);
+
+
+/** \brief Converts a name / string to a Weatherbit plan.
+ *
+ * \param planName  name of the plan
+ * \return Returns the enumeration value for the plan.
+ *         Returns PlanWeatherbit::none, if no match could be found.
+ */
+PlanWeatherbit toPlanWeatherbit(const std::string& planName);
+
+
+/** \brief enumeration type for plans of Weatherstack
+ */
+enum class PlanWeatherstack { none, Free, Standard, Professional, Business };
+
+
+/** \brief Gets the name of the Weatherstack plan.
+ *
+ * \param type  the plan
+ * \return Returns a string that identifies the plan.
+ */
+std::string toString(const PlanWeatherstack type);
+
+
+/** \brief Converts a name / string to a Weatherstack plan.
+ *
+ * \param planName  name of the plan
+ * \return Returns the enumeration value for the plan.
+ *         Returns PlanWeatherstack::none, if no match could be found.
+ */
+PlanWeatherstack toPlanWeatherstack(const std::string& planName);
+
+} // namespace
+
+#endif // WEATHER_INFORMATION_COLLECTION_PLANS_HPP
