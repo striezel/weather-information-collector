06d92ca6b51f27493a7b9010055284ce6cfcca0c
Dirk Stolle <striezel-dev@web.de>
2021-07-17 15:59:52 +0200

    more error handling tests for Weatherstack JSON parsing (simdjson)

--- tests/json/simdjson/SimdJsonWeatherstack.cpp
+++ tests/json/simdjson/SimdJsonWeatherstack.cpp
@@ -48,6 +48,413 @@ TEST_CASE("Class SimdJsonWeatherstack")
       REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather("\r\n\r\n", weather) );
       REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather("\t\t\t\t", weather) );
     }
+
+    SECTION("current element is missing")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "localtime":"2019-10-19 18:55",
+              "localtime_epoch":1571511300,
+              "utc_offset":"2.0"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    SECTION("current element is not an object")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "localtime":"2019-10-19 18:55",
+              "localtime_epoch":1571511300,
+              "utc_offset":"2.0"
+          },
+          "current": [ ]
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    SECTION("location element is missing")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    SECTION("location element is not an object")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location": "I am not an object, I am a string!",
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    /*
+
+    // Note: Format and range errors are not detected consistently.
+
+    SECTION("localtime has wrong format")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "localtime":"2019-10-19 de:ad",
+              "localtime_epoch":1571511300,
+              "utc_offset":"2.0"
+          },
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    SECTION("localtime's date is out of range")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "localtime":"2019-17-99 18:55",
+              "localtime_epoch":1571511300,
+              "utc_offset":"2.0"
+          },
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    SECTION("localtime's time is out of range")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "localtime":"2019-10-19 18:99",
+              "localtime_epoch":1571511300,
+              "utc_offset":"2.0"
+          },
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+    */
+
+    SECTION("localtime and localtime_epoch are missing")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "utc_offset":"2.0"
+          },
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
+
+    SECTION("localtime is missing and localtime_epoch (fallback) is not an integer")
+    {
+      const std::string json = R"json(
+      {
+          "request":
+          {
+              "type":"City",
+              "query":"Berlin, Germany",
+              "language":"en",
+              "unit":"m"
+          },
+          "location":
+          {
+              "name":"Berlin",
+              "country":"Germany",
+              "region":"Berlin",
+              "lat":"52.517",
+              "lon":"13.400",
+              "timezone_id":"Europe\/Berlin",
+              "localtime_epoch": "Not an integer",
+              "utc_offset":"2.0"
+          },
+          "current":
+          {
+              "observation_time":"04:55 PM",
+              "temperature":14,
+              "weather_code":113,
+              "weather_icons":
+              [
+                  "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png"
+              ],
+              "weather_descriptions":
+              [
+                  "Clear"
+              ],
+              "wind_speed":17,
+              "wind_degree":150,
+              "wind_dir":"SSE",
+              "pressure":1009,
+              "precip":0,
+              "humidity":82,
+              "cloudcover":0,
+              "feelslike":13,
+              "uv_index":0,
+              "visibility":10,
+              "is_day":"no"
+          }
+      }
+      )json";
+      REQUIRE_FALSE( SimdJsonWeatherstack::parseCurrentWeather(json, weather) );
+    }
   }
 
   SECTION("parseForecast")
