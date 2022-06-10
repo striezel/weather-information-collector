59af279e7c060c0f1cffc658a4dda9db88f5b95e
Dirk Stolle <striezel-dev@web.de>
2022-06-10 19:35:19 +0200

    use pkg-config to find libmariadb dependency

--- cmake/mariadb-config.cmake
+++ cmake/mariadb-config.cmake
@@ -5,7 +5,11 @@
 #  MARIADB_INCLUDE_DIR - The libmariadb include directories
 #  MARIADB_LIBRARIES - The libraries needed to use libmariadb
 
-find_path(MARIADB_INCLUDE_PATH mysql.h /usr/include/mariadb/)
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBMARIADB libmariadb)
+
+find_path(MARIADB_INCLUDE_PATH mysql.h
+          HINTS /usr/include/mariadb/ /mingw64/include/mariadb/ ${PC_LIBMARIADB_INCLUDE_DIRS})
 if(MARIADB_INCLUDE_PATH)
   set(MARIADB_INCLUDE_DIR
     ${MARIADB_INCLUDE_PATH}
@@ -14,7 +18,8 @@ else()
   message ( "Include path for libmariadb could not be determined!")
 endif(MARIADB_INCLUDE_PATH)
 
-find_library(MARIADB_LIBRARIES mariadb)
+find_library(MARIADB_LIBRARIES NAMES mariadb libmariadb
+             HINTS ${PC_LIBMARIADB_LIBRARY_DIRS})
 
 if (MARIADB_LIBRARIES)
 else (MARIADB_LIBRARIES)
