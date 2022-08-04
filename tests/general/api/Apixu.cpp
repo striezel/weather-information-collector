4bdd5cbc436570e388dbfe2c30701ec41efdabd5
Dirk Stolle <striezel-dev@web.de>
2022-08-04 23:25:21 +0200

    tests: add test cases for validLocation() method of APIs

---
+++ tests/general/api/Apixu.cpp
@@ -0,0 +1,76 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the test suite for weather-information-collector.
+    Copyright (C) 2022  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "../../find_catch.hpp"
+#include "../../../src/api/Apixu.hpp"
+
+TEST_CASE("Apixu - non-network tests")
+{
+  using namespace wic;
+
+  Apixu api("not a valid key");
+
+  SECTION("validLocation")
+  {
+    Location location;
+
+    SECTION("empty location")
+    {
+      REQUIRE_FALSE( api.validLocation(location) );
+    }
+
+    SECTION("OpenWeatherMap id")
+    {
+      location.setOwmId(123);
+      REQUIRE_FALSE( api.validLocation(location) );
+    }
+
+    SECTION("country code")
+    {
+      location.setCountryCode("DK");
+      REQUIRE_FALSE( api.validLocation(location) );
+    }
+
+    SECTION("latitude + longitude")
+    {
+      location.setCoordinates(12.34f, 56.78f);
+      REQUIRE( api.validLocation(location) );
+    }
+
+    SECTION("name is set")
+    {
+      location.setName("Some City");
+      REQUIRE( api.validLocation(location) );
+    }
+
+    SECTION("postcode is set")
+    {
+      location.setPostcode("ABC123");
+      REQUIRE( api.validLocation(location) );
+    }
+  }
+
+  SECTION("setApiKey")
+  {
+    // This is just a bogus test. Key is encapsulated, without getter, so there
+    // is no real way to check it.
+    api.setApiKey("foo1bar2baz3");
+  }
+}
