cd1b897778f3d37d88eeb9bf6c31990dab6720ab
Dirk Stolle <striezel-dev@web.de>
2022-08-19 14:38:49 +0200

    fix version information for task creator
    The version information does now include the used JSON library's
    version, too.

--- src/Version.cpp
+++ src/Version.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the weather information collector.
-    Copyright (C) 2017, 2018, 2019, 2020, 2021  Dirk Stolle
+    Copyright (C) 2017, 2018, 2019, 2020, 2021, 2022  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,13 +20,13 @@
 
 #include "Version.hpp"
 #include <iostream>
-#ifndef wic_no_json_parsing
+#if !defined(wic_no_json_parsing) || defined(wic_owm_find_location)
 #ifdef __SIZEOF_INT128__
 #include "../third-party/simdjson/simdjson.h"
 #else
 #include "../third-party/nlohmann/json.hpp"
 #endif // __SIZEOF_INT128__
-#endif // wic_no_json_parsing
+#endif // !defined(wic_no_json_parsing) || defined(wic_owm_find_location)
 #include "util/GitInfos.hpp"
 
 namespace wic
@@ -39,7 +39,7 @@ void showVersion(const std::string_view name)
             << "\n"
             << "Version control commit: " << info.commit() << "\n"
             << "Version control date:   " << info.date() << std::endl;
-#ifndef wic_no_json_parsing
+#if !defined(wic_no_json_parsing) || defined(wic_owm_find_location)
 #ifdef __SIZEOF_INT128__
   std::cout << "JSON library:           simdjson " << SIMDJSON_STRINGIFY(SIMDJSON_VERSION) << "\n"
             << "                        using implementation "
@@ -50,7 +50,7 @@ void showVersion(const std::string_view name)
             << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR
             << "." << NLOHMANN_JSON_VERSION_PATCH << std::endl;
 #endif // __SIZEOF_INT128__
-#endif // wic_no_json_parsing
+#endif // !defined(wic_no_json_parsing) || defined(wic_owm_find_location)
 
 }
 
